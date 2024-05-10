ce7767d4751c7edb695fbbb15aef0e53eacbb0a2
David Stupek <davidstupek@Davids-Air-2.lan>
2024-05-09 23:42:47 -0500

    Themes&StyledComps

--- knife/src/components/layout.js
+++ knife/src/components/layout.js
@@ -1,15 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
 import * as React from "react"
+import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled, { ThemeProvider } from "styled-components"
 
 import Header from "./header"
 import "./layout.css"
+/*This is so sensitive I had to copy the whole path to make it stick this time. But I had trouble with this in the homework and p1*/ 
+import { Blade } from "/Users/davidstupek/MATC/david-stupek-p2/david-stupek-p2/knife/src/themes/Blade/Blade.js"
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: var(--size-content);
+  padding: var(--size-gutter);
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -23,29 +26,29 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <ThemeProvider theme={Blade}>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
+      <Content>
         <main>{children}</main>
         <footer
           style={{
             marginTop: `var(--space-5)`,
             fontSize: `var(--font-sm)`,
           }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+          >
+            {new Date().getFullYear()} Built with 
+            {` `}
+            <a href="https://www.gatsbyjs.com">David Stupek & Gatsby</a>
+          </footer>
+      </Content>
+      </ThemeProvider>
   )
+      
 }
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+}
+      
 
 export default Layout
