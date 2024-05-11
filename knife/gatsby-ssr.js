887788e38ffeb172b5276dbef7165da018f4906e
David Stupek <davidstupek@Davids-Air-2.lan>
2024-05-11 16:57:55 -0500

    Backend&Paypal

--- knife/gatsby-ssr.js
+++ knife/gatsby-ssr.js
@@ -10,3 +10,10 @@
 exports.onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: `en` })
 }
+const React = require("react");
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script key="paypal-sdk" src="https://www.paypal.com/sdk/js?client-id=AZ031OYiwhtCuvQi0wCL7tbunHQzDesQ01DvVFQ4jZLZm8kCXVaLDsOOfXAu7qDeeYeR7CEzA6HwjdOZ&currency=USD"></script>
+  ]);
+};
